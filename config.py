4373d66ed020f92b32b27a7ba46b4c20d2f2c325
Erik Ramos <erikramos@Eriks-MBP.home>
2022-04-07 11:01:23 -0700

    edit1

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 
 from os import environ, path
 from dotenv import load_dotenv
-
+#production config
 class ProductionConfig():
     basedir = path.abspath(path.dirname(__file__))
     load_dotenv(path.join(basedir, '.env'))
